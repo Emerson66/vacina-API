3b8492ea1f688bc036d50d0bece48fb04a3e0e11
Emerson66 <emersonreisdacosta@gmail.com>
2021-03-13 08:32:50 -0400

    projeto criadoi

---
+++ src/test/java/com/emerson/vanina/api/VacinaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emerson.vanina.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacinaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
