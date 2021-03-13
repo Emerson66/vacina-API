d2a7e646aaea32c3e52c1110e8a919595166cbb6
Emerson66 <emersonreisdacosta@gmail.com>
2021-03-13 08:59:25 -0400

    Adicionado a classe Usuario

--- src/main/java/com/emerson/vacina/api/VacinaApiApplication.java
+++ src/main/java/com/emerson/vacina/api/VacinaApiApplication.java
@@ -1,4 +1,4 @@
-package com.emerson.vanina.api;
+package com.emerson.vacina.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
